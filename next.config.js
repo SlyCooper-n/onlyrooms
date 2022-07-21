1025d20b3ed2bcd426fd721cf8d91a7763697768
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-21 18:18:03 -0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+  // PWA configuration
+  const withPWA = require('next-pwa')
+  const runtimeCaching = require('next-pwa/cache')
+
+  module.exports = withPWA({
+    reactStrictMode: true,
+    pwa: {
+      dest: 'public',
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === 'development',
+      runtimeCaching,
+    },
+  })
---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import react from "@vitejs/plugin-react";
+  import tsconfigPaths from "vite-tsconfig-paths";
+  import { defineConfig } from "vitest/config";
+
+  export default defineConfig({
+    plugin: [tsconfigPaths(), react()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["./core/tests/setup.ts"],
+      coverage: {
+        enabled: true,
+        include: ["./components/**/*.tsx"],
+      },
+    },
+  });
