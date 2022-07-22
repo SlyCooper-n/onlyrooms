1dd63d3e6e4caebba6bc32d42612fcefe1a05015
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-21 23:42:16 -0300

    chore: vitest config set

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,15 @@
 import react from "@vitejs/plugin-react";
-  import tsconfigPaths from "vite-tsconfig-paths";
-  import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
 
-  export default defineConfig({
-    plugin: [tsconfigPaths(), react()],
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: ["./core/tests/setup.ts"],
-      coverage: {
-        enabled: true,
-        include: ["./components/**/*.tsx"],
-      },
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./core/tests/setup.ts"],
+    coverage: {
+      enabled: true,
+      include: ["./components/**/*.tsx"],
     },
-  });
+  },
+});
