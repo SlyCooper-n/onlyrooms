fb5a1522959cd7144a65aa608fed24347eb3575a
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-22 12:05:36 -0300

    chore: Button component created

---
+++ components/widgets/Button/Button.tsx
@@ -0,0 +1,22 @@
+import { ButtonProps } from "@core/types";
+
+export const Button = ({
+  type = "button",
+  ring = "",
+  className = "",
+  children,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      type={type}
+      className={`btn gap-2 font-primary ${className} ${
+        ring !== "" &&
+        `focus:ring-2 focus:ring-[${ring}] focus:ring-offset-4 focus:ring-offset-base-100`
+      }`}
+    >
+      {children}
+    </button>
+  );
+};
