288adf69a4f2e8cb0fa6f78ac6805f647cddbe09
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-22 12:10:26 -0300

    chore: add Auth provider and Toaster to every  page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AuthProvider } from "@core/contexts";
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import "../public/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+
+      <Toaster />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
