d39fffd080c58a820672791ec24dd67a502ac1b0
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-22 12:09:57 -0300

    chore: navbar menu options created

---
+++ core/utils/navbar-menu.ts
@@ -0,0 +1,30 @@
+export const navbarMenu = [
+  {
+    id: 1,
+    name: "Live questions",
+    link: "/rooms/live-questions",
+  },
+  {
+    id: 2,
+    name: "Anonymous questions",
+    link: "/rooms/anonymous-questions",
+  },
+  {
+    id: 3,
+    name: "Chat",
+    link: "/chat",
+  },
+];
+
+export const userOptions = [
+  {
+    id: 1,
+    name: "Profile",
+    link: "/account/profile",
+  },
+  {
+    id: 2,
+    name: "Settings",
+    link: "/account/settings",
+  },
+];
