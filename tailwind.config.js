4b378c67788c063f7fdad4c16f6ab4a4e52d60df
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-21 23:41:59 -0300

    chore: tailwind config set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import(tailwindcss).Config} */
-  module.exports = {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-      ],
-    theme: {
-      extend: {},
+
+module.exports = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Roboto"],
+        secondary: ["Poppins"],
+      },
     },
-    plugins: [
-      // require("@tailwindcss/forms"),
-      // require("tailwind-scrollbar"),
-      require("@tailwindcss/typography"),
-      require("daisyui"),
-    ],
-    daisyui: {
-      themes: ["dark", "light"],
-    }
-  }
+  },
+  plugins: [
+    // require("@tailwindcss/forms"),
+    // require("tailwind-scrollbar"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: ["dark", "light"],
+  },
+};
