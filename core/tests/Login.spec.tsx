99bdd68b28d20b7d4bd1fbbd69729810017cd1a3
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-22 15:23:27 -0300

    login page test created

---
+++ core/tests/Login.spec.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import Login from "pages/login";
+import { describe, expect, it, vi } from "vitest";
+
+vi.mock("@core/hooks", () => {
+  return {
+    useAuth: vi.fn(() => {
+      return {
+        user: {
+          id: "id",
+          name: "name",
+          avatar: "avatar",
+        },
+        signInWithGoogle: vi.fn(),
+      };
+    }),
+  };
+});
+
+describe("Login page", () => {
+  it("should render", () => {
+    render(<Login />);
+
+    expect(screen.getByText(/only/i)).toBeInTheDocument();
+  });
+
+  it("");
+});
