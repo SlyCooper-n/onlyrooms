ab4e6e6da09c5f2ce3a4c3082aaddd7ea626aae2
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-24 19:29:27 -0300

    chore: Illustration component added

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,4 +1,5 @@
 import { PageContainer } from "@components/layouts";
+import { Illustration } from "@components/modules";
 import { Button, Logo } from "@components/widgets";
 import { useAuth } from "@core/hooks";
 import { NextPage } from "next";
@@ -14,57 +15,58 @@ const Login: NextPage = () => {
   const { user, signInWithGoogle } = useAuth();
 
   return (
-    <PageContainer headTitle="OnlyRooms | Login" center>
-      <h1 className="mb-12 text-3xl font-bold font-secondary">
-        <Logo />
-      </h1>
+    <PageContainer headTitle="OnlyRooms | Login">
+      <section className="flex-1 flex items-center">
+        <Illustration className="flex-1 h-screen hidden lg:flex" />
 
-      <main className="text-center">
-        {user && (
-          <>
-            <h2 className="my-8 text-2xl font-semibold uppercase">
-              You are all set
-            </h2>
+        <main className="flex-1">
+          <h1 className="mb-12 text-center text-3xl font-bold font-secondary">
+            <Logo />
+          </h1>
 
-            <Link href="/">
-              <a className="link text-lg">Go to Home</a>
-            </Link>
-          </>
-        )}
+          <div className="text-center">
+            {user && (
+              <>
+                <h2 className="my-8 text-2xl font-semibold uppercase">
+                  You are all set
+                </h2>
+                <Link href="/">
+                  <a className="link text-lg">Go to Home</a>
+                </Link>
+              </>
+            )}
 
-        {!user && (
-          <div className="flex flex-col gap-4">
-            <Button
-              onClick={signInWithGoogle}
-              ring
-              className="bg-[#ea4435] text-white border-none hover:bg-[#ea4435] hover:brightness-90 focus:ring-[#ea4435]"
-            >
-              <GoogleLogo size={32} weight="bold" />
-              Login with Google
-            </Button>
-
-            <div className="divider">Coming soon...</div>
-
-            <Button disabled>
-              <GithubLogo size={32} />
-              Login with Github
-            </Button>
-
-            <Button disabled>
-              <AppleLogo size={32} />
-              Login with Apple
-            </Button>
-
-            <Button
-              disabled
-              className="bg-blue-500 text-white border-none hover:bg-blue-500 hover:brightness-90"
-            >
-              <FacebookLogo size={32} />
-              Login with Facebook
-            </Button>
+            {!user && (
+              <div className="flex flex-col gap-4">
+                <Button
+                  onClick={signInWithGoogle}
+                  ring
+                  className="bg-[#ea4435] text-white border-none hover:bg-[#ea4435] hover:brightness-90 focus:ring-[#ea4435]"
+                >
+                  <GoogleLogo size={32} weight="bold" />
+                  Login with Google
+                </Button>
+                <div className="divider">Coming soon...</div>
+                <Button disabled>
+                  <GithubLogo size={32} />
+                  Login with Github
+                </Button>
+                <Button disabled>
+                  <AppleLogo size={32} />
+                  Login with Apple
+                </Button>
+                <Button
+                  disabled
+                  className="bg-blue-500 text-white border-none hover:bg-blue-500 hover:brightness-90"
+                >
+                  <FacebookLogo size={32} />
+                  Login with Facebook
+                </Button>
+              </div>
+            )}
           </div>
-        )}
-      </main>
+        </main>
+      </section>
     </PageContainer>
   );
 };
