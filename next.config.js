2c71d67046f312d2c64b0597b9987950a122f4de
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-24 21:26:27 -0300

    chore: google images domain added

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
   pwa: {
     dest: "public",
     register: true,
