ffeffb0ee2249fd09b009ef192ad6595612ea5e6
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-24 22:32:58 -0300

    chore: move Lottie props inside Lottie component file

--- core/types/props/Lottie-props.ts
+++
@@ -1,9 +0,0 @@
-import { LottieOptions, LottieRefCurrentProps } from "lottie-react";
-
-export interface LottieProps extends LottieOptions {
-  speed?: number;
-  segments?: [number, number];
-  backwards?: boolean;
-  actionOnClick?: "revert" | "play-pause" | "stop" | "none";
-  customOnClick?: (lottieFunctions: LottieRefCurrentProps) => void;
-}
