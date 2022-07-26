ad3165eb5edf0b7b80ac2933acc0935c7f40de82
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-25 22:00:18 -0300

    chore: add transition when opening and closing

--- components/widgets/AvatarPopover/AvatarPopover.tsx
+++ components/widgets/AvatarPopover/AvatarPopover.tsx
@@ -1,40 +1,47 @@
 import { AvatarPopoverProps } from "@core/types";
 import { userOptions } from "@core/utils";
-import { Popover } from "@headlessui/react";
+import { Popover, Transition } from "@headlessui/react";
 import Image from "next/image";
 import Link from "next/link";
 
-export const AvatarPopover = ({ user }: AvatarPopoverProps) => {
+export const AvatarPopover = ({ user, className }: AvatarPopoverProps) => {
   return (
-    <Popover className="relative">
-      <Popover.Button className="avatar outline-none">
+    <Popover className={`relative ${className}`}>
+      <Popover.Button className="avatar outline-none focus:ring-2 focus:ring-accent-focus focus:ring-offset-2 focus:ring-offset-base-100">
         <div className="w-8 rounded-full">
           <Image
             src={user.avatar!}
             alt={`${user.name}'s avatar`}
             width={32}
             height={32}
-            layout="responsive"
           />
         </div>
       </Popover.Button>
 
-      <Popover.Panel
-        as="ul"
-        className="menu absolute top-10 right-0 bg-base-300 rounded-lg"
+      <Transition
+        enterFrom="opacity-0 translate-y-4"
+        enter="transition-all duration-150"
+        enterTo="opacity-100 -translate-y-2"
+        leave="transition-all duration-350"
+        leaveTo="opacity-0 translate-y-8"
       >
-        {userOptions.map((opt) => (
-          <li key={opt.id} className="">
-            <Link href={opt.link}>
-              <a className="flex justify-between gap-4">
-                {opt.name}
+        <Popover.Panel
+          as="ul"
+          className="menu absolute top-10 right-0 bg-base-300 rounded-lg z-20"
+        >
+          {userOptions.map((opt) => (
+            <li key={opt.id} className="">
+              <Link href={opt.link}>
+                <a className="flex justify-between gap-4">
+                  {opt.name}
 
-                {opt.icon}
-              </a>
-            </Link>
-          </li>
-        ))}
-      </Popover.Panel>
+                  {opt.icon}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </Popover.Panel>
+      </Transition>
     </Popover>
   );
 };
