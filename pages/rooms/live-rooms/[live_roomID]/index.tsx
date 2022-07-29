d5ad454206f92fbe9d5a3ad617a3647a53d02879
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-29 15:11:52 -0300

    chore: style header

--- pages/rooms/live-rooms/[live_roomID]/index.tsx
+++ pages/rooms/live-rooms/[live_roomID]/index.tsx
@@ -1,15 +1,38 @@
 import { PageContainer } from "@components/layouts";
-import { Logo } from "@components/widgets";
+import { Logo, RoomCode } from "@components/widgets";
+import { useRoom } from "@core/hooks";
 import { NextPage } from "next";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const LiveRoom: NextPage = () => {
-  const router = useRouter();
-  // const {} = useRoom(router.query.live_roomID);
+  const { live_roomID } = useRouter().query;
+  const { roomTitle, questions } = useRoom(live_roomID as string);
 
   return (
-    <PageContainer headTitle="OnlyRooms | Live Room">
-      <Logo />
+    <PageContainer headTitle={`OnlyRooms | ${roomTitle}`}>
+      <header className="py-2 bg-base-200">
+        <div className="container flex flex-col gap-4">
+          <Link href="/">
+            <a className="btn btn-ghost">
+              <h1 className="text-2xl font-bold text-center">
+                <Logo />
+              </h1>
+            </a>
+          </Link>
+
+          <RoomCode roomCode={live_roomID as string} />
+          {/* <ShareNetwork /> */}
+        </div>
+      </header>
+
+      <h2 className="text-xl text-center">
+        Room <span className="text-secondary">{roomTitle}</span>
+      </h2>
+
+      {/* <span>
+        {questions.likesCount}
+      </span> */}
     </PageContainer>
   );
 };
