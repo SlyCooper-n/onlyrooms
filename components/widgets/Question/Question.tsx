9e8a99cdff845d5ba5da49b1f9ee871597d97081
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-31 17:00:01 -0300

    chore: add admin variant

--- components/widgets/Question/Question.tsx
+++ components/widgets/Question/Question.tsx
@@ -3,8 +3,9 @@ import { useAuth } from "@core/hooks";
 import { db } from "@core/services";
 import { QuestionType } from "@core/types";
 import { FirebaseError } from "firebase/app";
-import { push, ref, remove } from "firebase/database";
+import { push, ref, remove, update } from "firebase/database";
 import { useRouter } from "next/router";
+import { Chat, Check, Trash } from "phosphor-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { Lottie } from "../Lottie";
@@ -12,27 +13,36 @@ import { UserInfo } from "../UserInfo";
 
 interface QuestionProps {
   data: QuestionType;
+  variant?: "admin" | "user";
 }
 
-export const Question = ({ data }: QuestionProps) => {
+export const Question = ({ data, variant = "user" }: QuestionProps) => {
   const { user } = useAuth();
   const [isLiked, setIsLiked] = useState(data.likeId !== null ? true : false);
+  const [isHighlighted, setIsHighlighted] = useState(data.isHighlighted);
+  const [isAnswered, setIsAnswered] = useState(data.isAnswered);
   const { live_roomID } = useRouter().query;
 
-  async function handleLike(likeId: string | null) {
+  const questionRef = ref(
+    db,
+    `rooms/live-rooms/${live_roomID}/questions/${data.id}`
+  );
+
+  async function handleLike() {
     if (!user) {
       toast.error("You must be logged in to like a question");
       return;
     }
 
     try {
-      if (isLiked && likeId !== null) {
+      if (isLiked && data.likeId !== null) {
         const likeIdRef = ref(
           db,
-          `rooms/live-rooms/${live_roomID}/questions/${data.id}/likes/${likeId}`
+          `rooms/live-rooms/${live_roomID}/questions/${data.id}/likes/${data.likeId}`
         );
 
         await remove(likeIdRef);
+        setIsLiked(false);
         return;
       }
 
@@ -44,6 +54,45 @@ export const Question = ({ data }: QuestionProps) => {
       await push(likeRef, {
         authorId: user.id,
       });
+      setIsLiked(true);
+    } catch (error) {
+      const err = error as FirebaseError;
+      toast.error(err.message);
+    }
+  }
+
+  async function handleHighlight() {
+    try {
+      await update(questionRef, {
+        isHighlighted: !isHighlighted,
+      });
+
+      setIsHighlighted(!isHighlighted);
+      return;
+    } catch (error) {
+      const err = error as FirebaseError;
+      toast.error(err.message);
+    }
+  }
+
+  async function handleAnswer() {
+    try {
+      await update(questionRef, {
+        isAnswered: !isAnswered,
+        isHighlighted: false,
+      });
+
+      setIsAnswered(!isAnswered);
+      setIsHighlighted(false);
+    } catch (error) {
+      const err = error as FirebaseError;
+      toast.error(err.message);
+    }
+  }
+
+  async function handleDelete() {
+    try {
+      await remove(questionRef);
     } catch (error) {
       const err = error as FirebaseError;
       toast.error(err.message);
@@ -51,29 +100,62 @@ export const Question = ({ data }: QuestionProps) => {
   }
 
   return (
-    <div className="card card-compact sm:min-h-[8rem] my-2 bg-base-200">
+    <div
+      className={`card card-compact sm:min-h-[8rem] my-2 ${
+        isHighlighted ? "bg-accent text-accent-content" : "bg-base-200"
+      } ${isAnswered ? "bg-base-200 opacity-70" : "bg-base-200"}`}
+    >
       <div className="card-body">
         <p className="sm:text-lg">{data.content}</p>
 
         <div className="flex justify-between items-center">
           <UserInfo />
 
-          <button
-            type="button"
-            className="ml-auto"
-            onClick={() => handleLike(data.likeId)}
-          >
-            <Lottie
-              animationData={likeButton}
-              segments={[0, 34]}
-              speed={1.75}
-              clicked={isLiked}
-              toggleClicked={() => setIsLiked(!isLiked)}
-              className="w-16 h-16"
-            />
-          </button>
+          {variant === "user" ? (
+            <>
+              <button type="button" className="ml-auto" onClick={handleLike}>
+                <Lottie
+                  animationData={likeButton}
+                  segments={[0, 34]}
+                  speed={1.75}
+                  clicked={isLiked}
+                  toggleClicked={() => setIsLiked(!isLiked)}
+                  className="w-16 h-16"
+                />
+              </button>
 
-          <span>{data.likesCount}</span>
+              <span>{data.likesCount}</span>
+            </>
+          ) : (
+            variant === "admin" && (
+              <div className="flex gap-2">
+                {!isHighlighted && !isAnswered && (
+                  <button type="button" onClick={handleHighlight}>
+                    <Chat
+                      size={24}
+                      className="w-4 sm:w-6 hover:text-secondary transition-colors"
+                    />
+                  </button>
+                )}
+
+                {!isAnswered && (
+                  <button type="button" onClick={handleAnswer}>
+                    <Check
+                      size={24}
+                      className="w-4 sm:w-6 hover:text-success transition-colors"
+                    />
+                  </button>
+                )}
+
+                <button type="button" onClick={handleDelete}>
+                  <Trash
+                    size={24}
+                    className="w-4 sm:w-6 hover:text-error transition-colors"
+                  />
+                </button>
+              </div>
+            )
+          )}
         </div>
       </div>
     </div>
